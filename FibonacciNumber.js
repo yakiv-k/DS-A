f8fd5cab7e159a62ba5f2c88af3189bd1a7d9213
yakiv-k <yakiv.y.k@gmail.com>
2023-03-27 11:21:37 -0400

    Add Fibonacci Number solution

---
+++ FibonacciNumber.js
@@ -0,0 +1,12 @@
+const fib = index => {
+    let fibSeq = [0, 1];
+  
+    for (let i = 0; i <= index; i++) {
+      if (!fibSeq[i-1] && !fibSeq[i-2]) {
+        continue;
+      } else {
+        fibSeq.push(fibSeq[i - 1] + fibSeq[i - 2]);
+      }
+    }
+    return fibSeq[index];
+  }
