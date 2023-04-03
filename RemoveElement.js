0632a0aefdef792c0f0068486118282ae2c67475
yakiv-k <yakiv.y.k@gmail.com>
2023-04-03 13:27:07 -0400

    Add Remove Element solution

---
+++ RemoveElement.js
@@ -0,0 +1,9 @@
+const removeElement = function (nums, val) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i, 1);
+          i -= 1;
+        }
+    }
+    return nums.length;
+}
