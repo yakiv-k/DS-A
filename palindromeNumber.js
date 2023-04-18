cbfea89d9a13150e05952cd99e4ae78818e929da
yakiv-k <yakiv.y.k@gmail.com>
2023-04-18 15:54:51 -0400

    Rename PalindromeNumber.js correctly

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -3,9 +3,10 @@ const isPalindrome = function (num) {
         let count = 0;
     
         for (let i = 0; i < numArr.length; i++) {
-            if (numArr[i] !== numArr.reverse()[i]) {
+            if (numArr[i] === numArr.reverse()[i] && count < 1) {
+            } else if (numArr[i] !== numArr.reverse()[i]) {
                 count += 1;
             }
         }
-        return count === 0 ? true : false;
+        return count < 1 ? true : false;
     };
