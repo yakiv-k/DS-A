7656059ef13b0aa815278d16b983fd34bc882a89
yakiv-k <yakiv.y.k@gmail.com>
2023-05-04 10:44:48 -0400

    Add Single Number solution(w/o bitwise)

---
+++ SingleNumber.js
@@ -0,0 +1,7 @@
+const singleNumber = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums.indexOf(nums[i]) === nums.lastIndexOf(nums[i])) {
+            return nums[i]
+        }
+    }
+};
