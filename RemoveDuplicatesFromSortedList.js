570d0933511b372d0e55493ab8a1e28d7211086b
yakiv-k <yakiv.y.k@gmail.com>
2023-07-07 09:30:57 -0400

    Add Remove Duplicates from Sorted Lists solution

---
+++ RemoveDuplicatesFromSortedList.js
@@ -0,0 +1,12 @@
+var deleteDuplicates = function(head) {
+    let current = head;
+
+    while (current) {
+        if (current.next !== null && current.val == current.next.val) {
+            current.next = current.next.next
+        } else {
+            current = current.next
+        }
+    }
+    return head
+};
